0f1cf57454ed9050b338062ef93a4d5c7ba0d749
canny-xi <331230985@qq.com>
2018-07-19 18:34:00 +0800

    x

--- src/page/Login/index.vue
+++ src/page/Login/index.vue
@@ -20,10 +20,11 @@
           </div>
         </div>
         <div class='textBtn'>
-          <span class='text' :class="current.type == '平面图' ? 'active' : ''" @click="clickTip(0 , '平面图')">平面图</span>
+          <span class='text' :class="current.type == tab ? 'active' : ''" @click="clickTip(index , tab)" v-for='(tab,index) in tabList'>{{tab}}</span>
+          <!-- <span class='text' :class="current.type == '平面图' ? 'active' : ''" @click="clickTip(0 , '平面图')">平面图</span>
           <span class='text' :class="current.type == '效果图' ? 'active' : ''" @click="clickTip(1 , '效果图')">效果图</span>
           <span class='text' :class="current.type == '3D图' ? 'active' : ''" @click="clickTip(2 , '3D图')">3D图</span>
-          <span class='text' :class="current.type == '实景图' ? 'active' : ''" @click="clickTip(3 , '实景图')">实景图</span>
+          <span class='text' :class="current.type == '实景图' ? 'active' : ''" @click="clickTip(3 , '实景图')">实景图</span> -->
         </div>
       </div>
       <div class='houseInfo'>
@@ -53,7 +54,9 @@ export default {
       names: [],
       listImg: [],
       show: true,
-      current: {}
+      current: {},
+      swiper: null,
+      tabList: []
     };
   },
 
@@ -62,10 +65,10 @@ export default {
   },
   methods: {
     swiperLoad() {
-      var swiper = new Swiper(".swiper-container", {
+      this.swiper = new Swiper(".swiper-container", {
         on: {
           slideChange: () => {
-            this.current = this.listImg[swiper.activeIndex]; //总图滑动 跳下tip
+            this.current = this.listImg[this.swiper.activeIndex]; //总图滑动 跳下tip
           }
         },
         observer: true
@@ -75,29 +78,22 @@ export default {
       this.show = false;
     },
     clickTip(index, name) {
-      var swiper = new Swiper(".swiper-container", {});
-      this.current.type = name;
-      console.log(this.current)
       let t1 = this.submitForm.img_data["平面图"].length;
       let t2 = this.submitForm.img_data["效果图"].length;
       let t3 = this.submitForm.img_data["3D图"].length;
       let t4 = this.submitForm.img_data["实景图"].length;
       if (index == 0) {
-        swiper.slideTo(1, 0, false);
+        this.swiper.slideTo(0, 0, false);
       }
       if (index == 1) {
-        swiper.slideTo(t1, 0, false);
+        this.swiper.slideTo(t1, 0, false);
       }
       if (index == 2) {
-        swiper.slideTo(t1 + t2, 0, false);
+        this.swiper.slideTo(t1 + t2, 0, false);
       }
       if (index == 3) {
-        swiper.slideTo(t1 + t2 + t3, 0, false);
+        this.swiper.slideTo(t1 + t2 + t3, 0, false);
       }
-
-      this.$nextTick(() => {
-        this.swiperLoad();
-      });
     },
     async getHouseType() {
       let res = await this.api.shareHouseType({
@@ -106,33 +102,15 @@ export default {
       if (res.code == 200) {
         this.submitForm = res.data;
         let arr = [];
-        let txt1 = res.data.img_data["平面图"];
-        for (let img of txt1) {
-          let temp = {};
-          temp.img = img;
-          temp.type = "平面图";
-          arr.push(temp);
-        }
-        let txt2 = res.data.img_data["效果图"];
-        for (let img of txt2) {
-          let temp = {};
-          temp.img = img;
-          temp.type = "效果图";
-          arr.push(temp);
-        }
-        let txt3 = res.data.img_data["3D图"];
-        for (let img of txt3) {
-          let temp = {};
-          temp.img = img;
-          temp.type = "3D图";
-          arr.push(temp);
-        }
-        let txt4 = res.data.img_data["实景图"];
-        for (let img of txt4) {
-          let temp = {};
-          temp.img = img;
-          temp.type = "实景图";
-          arr.push(temp);
+        for (let tab in res.data.img_data) {
+          this.tabList.push(tab);
+          let imgs = res.data.img_data[tab];
+          for (let img of imgs) {
+            let temp = {};
+            temp.type = tab;
+            temp.img = img;
+            arr.push(temp);
+          }
         }
         this.listImg = arr;
         this.current = this.listImg[0];
